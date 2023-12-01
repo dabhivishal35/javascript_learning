cc763ea3b3d62ea889856f0110174624dc283595
dabhivishal35 <152603656+dabhivishal35@users.noreply.github.com>
2023-12-01 10:15:36 +0000

    a

---
+++ basic_javascript/01_variables.js
@@ -0,0 +1,16 @@
+console.log("Parth")
+const accountId = 10
+console.log(accountId)
+
+let accountEmail = "parthvirani95@gmail.com"
+accountCity = "Parth"
+let accountState
+console.log(accountId);
+console.log(accountCity);
+
+accountEmail = "parth@gmail.com"
+console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountCity,accountState])
+
+console.log(accountState === undefined);
