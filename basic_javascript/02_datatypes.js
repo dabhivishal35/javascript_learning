7b613577a7d64d34502d5969862ae2f21a9b31cc
dabhivishal35 <152603656+dabhivishal35@users.noreply.github.com>
2023-12-01 10:23:27 +0000

    data types

--- basic_javascript/02_datatypes.js
+++ basic_javascript/02_datatypes.js
@@ -2,3 +2,20 @@
 
 // alert("Hello")   ///// we are using node js not  browser
 
+let name = "parth"
+let age = 18
+let isLoggedIn =false
+
+// number 
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
